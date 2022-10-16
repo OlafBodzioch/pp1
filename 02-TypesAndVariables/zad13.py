c446ccf6c34e4bc60ee5a7ce40e9dbb14e2c14ec
Olaf <226356@student.uek.krakow.pl>
2022-10-16 17:28:28 +0200

    y

---
+++ 02-TypesAndVariables/zad13.py
@@ -0,0 +1,22 @@
+import math
+#####
+# Calculation of the area and circumference of a circle
+##
+
+# determine radius and PI
+r = 5
+pi = math.pi
+
+# calculate area 
+area = r**2*pi
+
+# calculate circumference 
+circ = 2*r*pi
+
+# display results 
+print(f"The circle's area is: {area}")
+
+print(f"The circle's circumference is: {circ}")
+
+
+
---
+++ 02-TypesAndVariables/zad14.py
@@ -0,0 +1,8 @@
+x=input("Podaj temperaturę w jednostce C")
+x=int(x)
+
+fr=x*1.8+32
+
+k=273.15+x
+
+print(f"Temperatura {x} stopni C wynosi:\n{fr} F\n{k} K")
